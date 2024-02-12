d7fb0dfb54cb94541c16030f5e1b70aace38b94b
Hiwot Getasew Alemu <hiwot@spruce.rlogin>
2024-02-11 19:28:15 -0500

    Add front end

--- flask/server.py
+++ flask/server.py
@@ -1,16 +1,20 @@
-from flask import Flask
-#request, jsonify
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-#@app.route("/extract", methods=['POST'])
-@app.route("/extract")
-def extract():
-    #data = request.get_json()
-    return {"extract" : ["input your Url", "down below"]}
-    #return jsonify({ "message": "Data Extracted"})
-#extracting 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    # Process the uploaded file (save it, extract data, etc.)
+    # For simplicity, let's just return the filename for now
+    return jsonify({'filename': file.filename})
 
 #def anaylsis():
     
